e05c0e578f8a9a0b041817ce743338194daa7d69
muratkorkmazoglu <muratkorkmazoglu@gmail.com>
2017-11-22 17:57:52 +0300

    navigation drawer

--- MyNote/app/src/main/java/com/krkmz/mynote/MainActivity.java
+++ MyNote/app/src/main/java/com/krkmz/mynote/MainActivity.java
@@ -1,17 +1,30 @@
 package com.krkmz.mynote;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
@@ -21,13 +34,21 @@ import com.google.android.gms.ads.InterstitialAd;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import hotchemi.android.rate.AppRate;
+import hotchemi.android.rate.OnClickButtonListener;
+import hotchemi.android.rate.StoreType;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
     private ImageView newNote, allNote, tagImage, searchImage;
     String password;
     private AdView adView;
     private InterstitialAd gecisReklam;
-
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar toolbar;
+    private NavigationView navView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,17 +56,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_layout);
 
-        AdView adView = (AdView) this.findViewById(R.id.adView);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        adView = (AdView) this.findViewById(R.id.adView);
+        navView = (NavigationView) findViewById(R.id.nav_view);
+        mToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navView.setNavigationItemSelectedListener(this);
+
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
 
         gecisReklam = new InterstitialAd(this);
-
         gecisReklam.setAdUnitId("ca-app-pub-3924428861396897/5880807524");
-
         gecisReklam.loadAd(new AdRequest.Builder().build());
-
-        gecisReklam.setAdListener(new AdListener(){
+        gecisReklam.setAdListener(new AdListener() {
             @Override
             public void onAdClosed() {
                 super.onAdClosed();
@@ -53,11 +81,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         SharedPreferences preferences = getSharedPreferences("PREFS", 0);
         password = preferences.getString("password", "0");
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");
         Date resultdate = new Date(System.currentTimeMillis());
@@ -107,20 +133,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.updatePassword) {
-            Intent ıntent = new Intent(getApplicationContext(), NewPasswordActivity.class);
-            ıntent.putExtra("password",password);
-            startActivity(ıntent);
-            MainActivity.this.finish();
+
+        if (mToggle.onOptionsItemSelected(item)) {
             return true;
         }
+        switch (item.getItemId()) {
+
+            case R.id.updatePassword:
+                Intent ıntent = new Intent(getApplicationContext(), NewPasswordActivity.class);
+                ıntent.putExtra("password", password);
+                startActivity(ıntent);
+                MainActivity.this.finish();
+                return true;
 
 
-        return super.onOptionsItemSelected(item);
-
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
-    public void showInterstitial(){
+    public void showInterstitial() {
         if (gecisReklam.isLoaded()) {
             gecisReklam.show();
         } else {
@@ -132,5 +163,55 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         showInterstitial();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.rate:
+                AppRate.with(MainActivity.this)
+                        .setStoreType(StoreType.GOOGLEPLAY) //default is Google, other option is Amazon
+                        .setInstallDays(3) // default 10, 0 means install day.
+                        .setLaunchTimes(10) // default 10 times.
+                        .setRemindInterval(2) // default 1 day.
+                        .setShowLaterButton(true) // default true.
+                        .setDebug(true) // default false.
+                        .setCancelable(false) // default false.
+                        .setOnClickButtonListener(new OnClickButtonListener() { // callback listener.
+                            @Override
+                            public void onClickButton(int which) {
+                                Log.d(MainActivity.class.getName(), Integer.toString(which));
+                            }
+                        })
+                        .setTitle("Oy Ver")
+                        .setTextLater("Daha Sonra Hatırlat")
+                        .setTextNever("Asla")
+                        .setTextRateNow("Tamam")
+                        .monitor();
+
+                AppRate.showRateDialogIfMeetsConditions(MainActivity.this);
+                drawerLayout.closeDrawer(Gravity.LEFT);
+
+
+                break;
+            case R.id.contact:
+
+                break;
+
+            case R.id.share:
+                try {
+                    Intent i = new Intent(Intent.ACTION_SEND);
+                    i.setType("text/plain");
+                    i.putExtra(Intent.EXTRA_SUBJECT, "My Note");
+                    String sAux = "\nBu Uygulamayı Severek Kullanıyorum ve Sanada Tavsiye Ediyorum.\n\n";
+                    sAux = sAux + "https://play.google.com/store/apps/details?id=Orion.Soft \n\n";
+                    i.putExtra(Intent.EXTRA_TEXT, sAux);
+                    startActivity(Intent.createChooser(i, "Paylaş"));
+                } catch (Exception e) {
+                    e.toString();
+                }
+                break;
+        }
+        return true;
     }
 }
